2ed2e44709e88e70bf0c8d470f7166de41f939c0
harunakgun <harun.akgun@limango.com.tr>
2013-09-06 21:05:08 +0300

    as

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once('inc/db_con.php');
-$resuls = mysql_db_query($db,'SELECT * FROM raw_materials',$cn);
+$results = mysql_db_query($db,'SELECT * FROM raw_materials',$cn);
 while($row = mysql_fetch_assoc($results)){
 	var_dump($row);
 }
