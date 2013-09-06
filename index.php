6306211e3be1f4e20be73c78f30680ccf31acc4e
harunakgun <harun.akgun@limango.com.tr>
2013-09-06 21:45:46 +0300

    master

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 include_once('inc/db_con.php');
-if($_GET['material_name'] && $_GET['material_name'] != "" && isset($_FILES["file"]["name"])){
-	$last_insert = mysql_db_query($db,"INSERT INTO raw_materials VALUES('','".$_GET['material_name']."','0','0','0')",$cn);
+if($_POST['material_name'] && $_POST['material_name'] != "" && isset($_FILES["file"]["name"])){
+	$last_insert = mysql_db_query($db,"INSERT INTO raw_materials VALUES('','".$_POST['material_name']."','0','0','0')",$cn);
 	if($last_insert){
 		$filename = $_FILES["file"]["name"];
 		$file_ext = explode('.',$_FILES["file"]["name"]);
 		$file_ext = $file_ext[1];
 		move_uploaded_file($_FILES["file"]["tmp_name"],"/media/raw_materials/" . $last_insert.'.'.$file_ext);
-		echo($_GET['material_name'].'('.$last_insert.') succesfully inserted.<br>');
+		echo($_POST['material_name'].'('.$last_insert.') succesfully inserted.<br>');
 	}
 }
 ?>
