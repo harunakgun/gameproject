794ef1869177e808d09367092c54b8fa125e2b86
harunakgun <harun.akgun@limango.com.tr>
2013-09-06 21:24:41 +0300

    as

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 include_once('inc/db_con.php');
+if($_GET['material_name'] && $_GET['material_name'] != ""){
+	$last_insert = mysql_db_query($db,"INSERT INTO raw_materials VALUES('','".$_GET['material_name']."','0','0','0')",$cn);
+	if($last_insert){
+		echo($_GET['material_name'].'('.$last_insert.') succesfully inserted.<br>');
+	}
+}
+?>
+<form action="index.php">
+	<input type="text" value="" name="material_name">
+	<input type="submit" value="submit">
+</form>
+<?
 $results = mysql_db_query($db,'SELECT * FROM raw_materials',$cn);
 while($row = mysql_fetch_assoc($results)){
-	var_dump($row);
+	echo($row['name']."<br>");
 }
 ?>
