fa40a4b9c68545f87dc8b7742eb4417a1a72ac64
harunakgun <harun.akgun@limango.com.tr>
2013-09-07 16:05:23 +0300

    m

--- inc/db_con.php
+++ inc/db_con.php
@@ -3,5 +3,8 @@
 $db="eco_mass";
 $cn = mysql_connect("localhost", "root", "ha4518");
 mysql_select_db($db, $cn);
+mysql_query("SET NAMES 'utf8'");
+mysql_query("SET CHARACTER SET utf8");
+mysql_query("SET COLLATION_CONNECTION = 'utf8-general-ci'");
 
 ?>
