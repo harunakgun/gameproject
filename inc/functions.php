79622b65f5d842aaef7a853e30d8809534c7e428
harunakgun <harun.akgun@limango.com.tr>
2013-09-08 11:59:41 +0300

    master

--- inc/functions.php
+++ inc/functions.php
@@ -16,9 +16,9 @@ function make_thumb($image_name,$new_image_name,$thumb_width =40,$quality = 90)
      $imgratio=$breite/$hoehe;
      $newwidth = $ThumbWidth;
      $newheight = $ThumbWidth/$imgratio;
-     imagealphablending( $image2, false );
-     imagesavealpha( $image2, true );
      $image2 = imagecreatetruecolor($newwidth, $newheight);
+	 imagealphablending( $image2, false );
+     imagesavealpha( $image2, true );
      imagecopyresampled ($image2, $image, 0, 0, 0, 0, $newwidth, $newheight, $breite, $hoehe);
      @imagepng($image2,$new_image_name , $quality);
      imagedestroy($image);
