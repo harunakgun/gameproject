7a6eb35b0dd4a61e28872e08f124fe83c4cdf6bf
harunakgun <harun.akgun@limango.com.tr>
2013-09-08 11:45:06 +0300

    master

--- factory_insert.php
+++ factory_insert.php
@@ -3,6 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 $dc_root = $_SERVER['DOCUMENT_ROOT'];
 
 include_once('inc/db_con.php');
+include_once('inc/functions.php');
 if($_POST['factory_name'] && $_POST['factory_name'] != "" && isset($_FILES["file"]["name"])){
 	$success = mysql_db_query($db,"INSERT INTO factories VALUES('','".$_POST['factory_name']."','0','0','0')",$cn);
 	if($success){
@@ -10,7 +11,10 @@ if($_POST['factory_name'] && $_POST['factory_name'] != "" && isset($_FILES["file
 		$filename = $_FILES["file"]["name"];
 		$file_ext = explode('.',$_FILES["file"]["name"]);
 		$file_ext = $file_ext[1];
-		move_uploaded_file($_FILES["file"]["tmp_name"],$dc_root."media/factories/" . $last_insert.'.'.$file_ext);
+		$filePath = $dc_root."media/factories/big/" . $last_insert.'.'.$file_ext;
+		$thumbPath = $dc_root."media/factories/thumb/" . $last_insert.'.'.$file_ext;
+		move_uploaded_file($_FILES["file"]["tmp_name"],$filePath);
+		make_thumb($filePath,$thumbPath,64,100);
 		echo($_POST['material_name'].'('.$last_insert.') succesfully inserted.<br>');
 	}
 }
@@ -24,10 +28,8 @@ if($_POST['factory_name'] && $_POST['factory_name'] != "" && isset($_FILES["file
 $results = mysql_db_query($db,'SELECT * FROM factories',$cn);
 while($row = mysql_fetch_assoc($results)){
 	$imgPath = "";
-	if(file_exists($dc_root.'media/factories/'.$row['id'].'.png')) {
-		$imgPath = '/media/factories/'.$row['id'].'.png';
-	} elseif(file_exists($dc_root.'media/factories/'.$row['id'].'.jpg')) {
-		$imgPath = '/media/factories/'.$row['id'].'.jpg';
+	if(file_exists($dc_root.'media/factories/thumbs/'.$row['id'].'.png')) {
+		$imgPath = '/media/factories/thumbs/'.$row['id'].'.png';
 	}
 	echo("<div style='width:200px;height:64px;float:left;border:1px solid #ccc;'><div style='height:64px;width:64px;float:left;'><img src='".$imgPath."' /></div><div style='float:left;'>".$row['name']."</div></div>");
 }
---
+++ inc/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+function make_thumb($image_name,$new_image_name,$thumb_width =40,$quality = 90)
+{
+    if(!file_exists($image_name)){ echo('<b>bad:'.$image_name.'</b><br>'); return; }
+     if(substr_count($image_name,'.png')>0){
+         $image = imagecreatefrompng($image_name);
+     }else{
+         $image = imagecreatefromjpeg($image_name);
+     }
+
+     $breite = imagesx($image);
+     $hoehe = imagesy($image);
+
+     $ThumbWidth = $thumb_width;
+     $imgratio=$breite/$hoehe;
+     $newwidth = $ThumbWidth;
+     $newheight = $ThumbWidth/$imgratio;
+
+     $image2 = imagecreatetruecolor($newwidth, $newheight);
+     imagecopyresampled ($image2, $image, 0, 0, 0, 0, $newwidth, $newheight, $breite, $hoehe);
+     @imagejpeg($image2,$new_image_name , $quality);
+     imagedestroy($image);
+     imagedestroy($image2);
+     @chmod($new_image_name, 0777);
+}
+
+?>
