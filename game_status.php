3366eca3b45092da10a5e2cdcfac7cfd833e95be
harunakgun <harun.akgun@limango.com.tr>
2013-09-09 20:33:57 +0300

    a

--- game_status.php
+++ game_status.php
@@ -4,6 +4,16 @@ include_once('inc/db_con.php');
 include_once('inc/functions.php');
 $results = mysql_db_query($db,'SELECT * FROM system WHERE ended_at=0 LIMIT 1',$cn);
 $row = mysql_fetch_array($results);
-echo($row[0]['started_at']."-".$row['started_at']."#");
+$serverStarted = $row['started_at'];
+$currentTime = time();
 
+$passedSecs = $currentTime - $serverStarted;
+$interval = 5;
+$minutes = $passedSecs / $interval;
+
+$d = floor ($minutes / 1440);
+$h = floor (($minutes - $d * 1440) / 60);
+$m = $minutes - ($d * 1440) - ($h * 60);
+
+echo "{$minutes}min converts to {$d}d {$h}h {$m}m";
 ?>
