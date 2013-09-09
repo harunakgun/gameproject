1eab792343b384442bd632b4d4b390f5679eef29
harunakgun <harun.akgun@limango.com.tr>
2013-09-09 20:27:41 +0300

    a

---
+++ game_status.php
@@ -0,0 +1,9 @@
+﻿<?php
+header('Content-Type: text/html; charset=utf-8');
+include_once('inc/db_con.php');
+include_once('inc/functions.php');
+$results = mysql_db_query($db,'SELECT * FROM system WHERE ended_at=0 LIMIT 1',$cn);
+$row = mysql_fetch_array($results);
+var_dump($row);
+
+?>
