da9f907e2bdd1961b43c773aa4a7c29006d3e1b4
harunakgun <harun.akgun@limango.com.tr>
2013-09-09 20:43:40 +0300

    a

--- game_status.php
+++ game_status.php
@@ -11,9 +11,14 @@ $passedSecs = $currentTime - $serverStarted;
 $interval = 5;
 $minutes = $passedSecs / $interval;
 
-$d = floor ($minutes / 1440);
-$h = floor (($minutes - $d * 1440) / 60);
-$m = $minutes - ($d * 1440) - ($h * 60);
+//NOW CONVERT THE MINUTES TO SECONDS
+$totalSecs = ($minutes/60);
 
-echo "{$minutes}min converts to {$d}d {$h}h {$m}m";
+$monthToShow = date("F",$totalSecs);
+$dayToShow = date("D",$totalSecs);
+$yearToShow = (date("Y",$totalSecs)) - 70;
+$hoursToShow = date("H:i",$totalSecs);
+$date = $monthToShow." ".$dayToShow.", ".$yearToShow.", ".$hoursToShow;
+
+echo $date;
 ?>
