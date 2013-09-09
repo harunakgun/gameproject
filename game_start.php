2f6b11123653ffb9534b00d4806c54c3ca5347f9
harunakgun <harun.akgun@limango.com.tr>
2013-09-09 20:25:06 +0300

    a

--- game_start.php
+++ game_start.php
@@ -3,6 +3,6 @@ header('Content-Type: text/html; charset=utf-8');
 include_once('inc/db_con.php');
 include_once('inc/functions.php');
 $success = mysql_db_query($db,"INSERT INTO system VALUES('','".time()."','0')",$cn);
-echo();
+echo($success);
 	
 ?>
